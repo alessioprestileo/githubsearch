2a9d201a3561fc9b94f791720d5f3aee532a697c
Alessio Prestileo <alessioprestileo@gmail.com>
2022-04-24 23:16:57 +0200

    Start implementing github functions and improve formatting
    Try logging current github user. Enforce single quotes when formatting.

--- netlify/functions/hello-world/hello-world.ts
+++ netlify/functions/hello-world/hello-world.ts
@@ -1,7 +1,7 @@
-import { Handler } from "@netlify/functions";
+import { Handler } from '@netlify/functions';
 
-export const handler: Handler = async (event, context) => {
-  const { name = "stranger" } = event.queryStringParameters || {};
+export const handler: Handler = async (event) => {
+  const { name = 'stranger' } = event.queryStringParameters || {};
 
   return {
     statusCode: 200,
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
@@ -18,6 +18,6 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-fetch("/.netlify/functions/hello-world?name=sarah")
+fetch('/.netlify/functions/github')
   .then((res) => res.json())
-  .then((data) => console.log("data = ", data));
+  .then((data) => console.log('data = ', data));
